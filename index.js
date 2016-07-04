bee3badfe788dc9a3c2adb355ad2af1d571a42fa
arkenidar <dario.cangialosi@gmail.com>
2016-07-04 22:00:12 +0200

    code beautyfication

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*
-
-changes in this commit:
-offline/hotseat mode working again, changed circle insertion now possible everywhere in column, added color of first turn in new game, winning circles blink animation still present after page reload (winning circles state is better handled), new game is not automatically initiated (because it require choosing the color that plays first turn).
-
-todo: code need beautification (naming, erasing, spacing)
-
-*/
-
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -22,17 +13,17 @@ app.get('/', function (req, res) {
   res.redirect('/connect_four.html');
 });
 
-const size_x = 7, size_y = 6;
+const sizeX = 7, sizeY = 6;
 const colors = ['blue', 'green'];
 var color;
 
 // grid operation
-function empty_grid(){
+function emptyGrid() {
   grid = [];
-  for(var x=0; x<size_x; x++){
+  for (var x = 0; x < sizeX; x++) {
     var column = [];
-    for(var y=0; y<size_y; y++){
-      column.push({color:'white', blink:false});
+    for (var y = 0; y < sizeY; y++) {
+      column.push({ color: 'white', blink: false });
     }
     grid.push(column);
   }
@@ -40,11 +31,11 @@ function empty_grid(){
 }
 
 // grid operation
-function insert(column_index){
-  var column = grid[column_index];
+function insert(columnIndex) {
+  var column = grid[columnIndex];
   var inserted = false;
-  for(var y=(size_y-1); y>=0; y--){
-    if(column[y].color === 'white'){
+  for (var y = (sizeY - 1); y >= 0; y--) {
+    if (column[y].color === 'white') {
       column[y].color = color;
       inserted = true;
       break;
@@ -53,116 +44,111 @@ function insert(column_index){
   return inserted;
 }
 
-function valid_indices(x, y){
-  return (x>=0 && x<size_x) && (y>=0 && y<size_y);
+function validIndices(x, y) {
+  return (x >= 0 && x < sizeX) && (y >= 0 && y < sizeY);
 }
 
 const directions = [
-    [0,1],
-    [1,0],
-    [0,-1],
-    [-1,0],
-    [1,1],
-    [-1,-1],
-    [-1, 1],
-    [1, -1]
-  ];
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+  [1, 1],
+  [-1, -1],
+  [-1, 1],
+  [1, -1]
+];
 
 // grid operation
-function check_pos_dir(x, y, direction){
-  var x2=x;
-  var y2=y;
+function checkPosDir(x, y, direction) {
+  var x2 = x;
+  var y2 = y;
   var circles = [];
-  for(var c=0; c<4; c++){
+  for (var c = 0; c < 4; c++) {
     x2 += direction[0];
     y2 += direction[1];
-    if(!valid_indices(x2, y2)) return false;
-    if(grid[x2][y2].color!==color) return false;
-    //circles.push([x2, y2]);
-    //grid[x2][y2].blink = true;
+    if (!validIndices(x2, y2)) return false;
+    if (grid[x2][y2].color !== color) return false;
     circles.push([x2, y2]);
   }
-  //var circles = [];
-    for(var i=0; i<4; i++){
-      var pos = circles[i];
-      var x = pos[0];
-      var y = pos[1];
-      //grid[x][y]='black';
-      //circles.push([x, y]);
-      grid[x][y].blink = true;
-    }
+  for (var i = 0; i < 4; i++) {
+    var pos = circles[i];
+    var x = pos[0];
+    var y = pos[1];
+    grid[x][y].blink = true;
+  }
   return true;
 }
 
-function check_endgame(){
-  for(var x=0; x<size_x; x++){
-    for(var y=0; y<size_y; y++){
-      for(var dir_i=0; dir_i<directions.length; dir_i++){
-        var direction = directions[dir_i];
-        var result = check_pos_dir(x,y, direction);
-        if(result) return true;
+function checkEndGame() {
+  for (var x = 0; x < sizeX; x++) {
+    for (var y = 0; y < sizeY; y++) {
+      for (var dirI = 0; dirI < directions.length; dirI++) {
+        var direction = directions[dirI];
+        var result = checkPosDir(x, y, direction);
+        if (result) return true;
       }
     }
   }
   return false;
 }
 
-function new_game(firstTurnColor){
-  grid = empty_grid();
+function newGame(firstTurnColor) {
+  grid = emptyGrid();
   win = null;
   color = firstTurnColor;
-  game_in_progress = true;
+  gameInProgress = true;
 }
 
-function initialState(){
-  grid = empty_grid();
+function initialState() {
+  grid = emptyGrid();
   win = null;
   color = null;
-  game_in_progress = false;
+  gameInProgress = false;
 }
 initialState();
 
 io.on('connection', function (socket) {
-  function on_new_game_server(data){
-    new_game(data.color);
-    socket.broadcast.emit('new_game_propagated', {color});
+  function onNewGameServer(data) {
+    newGame(data.color);
+    socket.broadcast.emit('newGamePropagated', { color });
   }
-  socket.on('new_game_server', on_new_game_server);
+  socket.on('newGameServer', onNewGameServer);
 
-  function on_insert_request(data){
+  function onInsertRequest(data) {
     console.log('on insert');
     var column = data.column;
 
     // able to insert, inserted
-    socket.broadcast.emit('insert_propagated', data);
-    
+    socket.broadcast.emit('insertPropagated', data);
+
     // is game in progress?
-    if(win) return;
+    if (win) return;
 
     // try to insert
     var inserted = insert(column);
-    if(!inserted){
+    if (!inserted) {
       // unable to insert
       console.log('full');
       return;
-    }else{      
+    } else {
       // endgame
-      var end = check_endgame();
-      if(end){
-        console.log(color+' wins');
+      var end = checkEndGame();
+      if (end) {
+        console.log(color + ' wins');
         win = color;
       }
     }
     // turns
-    color = colors[(colors.indexOf(color)+1)%(colors.length)];
+    color = colors[(colors.indexOf(color) + 1) % (colors.length)];
   }
-  socket.on('insert_request', on_insert_request);
-  
-  function on_load_page_request(){
-    console.log('on_load_page_request');
-    data = {win, color, grid, game_in_progress};
-    socket.emit('load_page_response', data);
+  socket.on('insertRequest', onInsertRequest);
+
+  function onLoadPageRequest() {
+    console.log('onLoadPageRequest');
+    data = { win, color, grid, gameInProgress };
+    socket.emit('loadPageResponse', data);
   }
-  socket.on('load_page_request', on_load_page_request);
-  
+  socket.on('loadPageRequest', onLoadPageRequest);
+
 });
